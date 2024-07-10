0e10001e48cd7101bf5164142244446fa065e4fe
karansain <karansainnn@gmail.com>
2024-07-10 19:45:14 +0530

    Page Done

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# OctaNet
+# CodSoft
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                'roboto': ['Roboto', 'sans-serif'],
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
